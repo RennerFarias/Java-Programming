ea2bf76d64d4c28919aa286d6fbef6138d09b291
Renner <rennerbarbosadefarias76@gmail.com>
2025-04-10 18:52:57 -0300

    creating the java folder
    learning about primitive data in java

---
+++ src/TiposDeDados/DadosPrimitivos.java
@@ -0,0 +1,18 @@
+package TiposDeDados;
+
+public class DadosPrimitivos {
+    public static void main(String[] args) {
+
+        /*
+        *Dados Primitivos - int, double, float, char, boolean, short.
+        *Objetivo da aula: Criar um ninja
+         */
+
+        int idade = 16;
+        double altura = 1,65
+        char inicial = 'N';
+        boolean VivoOuMorto = true;
+        Long SaldoBancario = 9999999L;
+
+    }
+}
